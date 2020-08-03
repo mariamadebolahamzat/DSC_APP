a1f4bc9e9a42c8b4b7181c7bd7f107292b8f3f67
Mariam Hamzat <titilayobolamide247@gmail.com>
2020-08-03 12:57:57 +0100

    your commit here

---
+++ lib/UI/events.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class EventApp extends StatelessWidget{
+   @override
+   Widget build (BuildContext context){
+     return MaterialApp(
+       home: Scaffold(
+         backgroundColor: Colors.white,
+         body: Container(),
+         ),
+     );
+   }
+}
